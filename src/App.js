334098ea71a3942d78fa214c3965d82fd01acfea
Georgi <figoto660@gmail.com>
2022-09-19 14:54:46 -0400

    adding logic

--- src/App.js
+++ src/App.js
@@ -1,36 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import FromStyle from "./style/FormStyle";
 
 function App() {
-  return (
+  const [formValue, setFormValue] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+  });
+  const [submit, setSubmit] = useState(false);
 
-        <FromStyle>
-          <h1>Form</h1>
-          <input
-            id="first-name"
-            className="form-field"
-            type="text"
-            placeholder="First Name"
-            name="firstName"
-          />
-          <input
-            id="last-name"
-            className="form-field"
-            type="text"
-            placeholder="Last Name"
-            name="lastName"
-          />
-          <input
-            id="email"
-            className="form-field"
-            type="text"
-            placeholder="Email"
-            name="email"
-          />
-          <button className="form-field submit" type="submit">
-            Register
-          </button>
-        </FromStyle>
+  const inputChangeFistName = (e) => {
+    setFormValue({ ...formValue, firstName: e.target.value });
+  };
+  const inputChangeLastName = (e) => {
+    setFormValue({ ...formValue, lastName: e.target.value });
+  };
+  const inputChangeEmail = (e) => {
+    setFormValue({ ...formValue, email: e.target.value });
+  };
+  const inputChangePhoneNumber = (e) => {
+    setFormValue({ ...formValue, phoneNumber: e.target.value });
+  };
+
+  const runSubmit = (e) => {
+    e.preventDefault();
+    if (formValue.firstName && formValue.lastName && formValue.email) {
+      setSubmit(!submit);
+      setTimeout(() => {
+        setSubmit(!submit);
+      }, 1000);
+      setFormValue({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+      });
+
+    } else {
+      alert("Some information is missing");
+    }
+  };
+
+
+
+  return (
+    <FromStyle onSubmit={runSubmit}>
+      <h1>Form</h1>
+      {submit ? (
+        <div className="form-register">Thank you for registering!</div>
+      ) : null}
+      <input
+        onChange={inputChangeFistName}
+        value={formValue.firstName}
+        id="first-name"
+        className="form-field"
+        type="text"
+        placeholder="First Name*"
+        name="firstName"
+      />
+      {!formValue.firstName ? <p>Please enter your First Name</p> : ""}
+      <input
+        onChange={inputChangeLastName}
+        value={formValue.lastName}
+        id="last-name"
+        className="form-field"
+        type="text"
+        placeholder="Last Name*"
+        name="lastName"
+      />
+      {!formValue.lastName ? <p>Please enter your Last Name</p> : ""}
+      
+      <input
+        onChange={inputChangeEmail}
+        value={formValue.email}
+        id="email"
+        className="form-field"
+        type="text"
+        placeholder="Email*"
+        name="email"
+      />
+      {!formValue.email ? <p>Please enter your email</p> : ""}
+      <input
+        onChange={inputChangePhoneNumber}
+        value={formValue.phoneNumber}
+        id="phone"
+        className="form-field"
+        type="tel"
+        placeholder="Phone Number(optional)"
+        name="phone"
+      />
+      <button className="form-field submit" type="submit">
+        Register
+      </button>
+    </FromStyle>
   );
 }
 
--- src/style/FormStyle.js
+++ src/style/FormStyle.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 const FromStyle = styled.form`
   background-color: #ee4e34;
-  border-radius: 3rem;
   margin: auto;
   margin-top: 10rem;
   text-align: center;
@@ -11,12 +10,18 @@ const FromStyle = styled.form`
   max-width: 450px;
   box-shadow: 20px 20px 50px 15px grey;
 
+  .form-register{
+    color: white;
+    background-color: green;
+    margin: auto;
+    padding: 0.3rem;
+  }
   .form-field {
     margin: 1rem 5rem;
     height: 2rem;
     border-radius: 0.5rem;
     padding-left: 0.5rem;
-    font-size: 1.5rem;
+    font-size: 1.3rem;
     border: black 1px solid;
     font-family: 'Open Sans', sans-serif;
   }
